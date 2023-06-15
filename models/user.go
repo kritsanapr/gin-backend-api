619a1392556931ff6171cab272463ff8f83d47ca
kritsanapr <kritsana.pr.41@gmail.com>
2023-06-15 15:43:04 +0700

    feat : add blog model and add relation one to many

--- models/user.go
+++ models/user.go
@@ -13,6 +13,7 @@ type User struct {
 	Email     string    `json:"email" gorm:"type:varchar(255); not null; unique"`
 	Password  string    `json:"-" gorm:"type:varchar(255); not null"`
 	IsAdmin   bool      `json:"is_admin" gorm:"type:bool; default:false"`
+	Blogs     []Blog    `json:"blogs" gorm:"foreignKey:UserID"`
 	CreatedAt time.Time `json:"created_at" gorm:"type:timestamp; not null; default:CURRENT_TIMESTAMP"`
 	UpdatedAt time.Time `json:"updated_at" gorm:"type:timestamp; not null; default:CURRENT_TIMESTAMP"`
 }
